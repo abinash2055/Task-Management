a38ca505a84a9073a50d4e18588fe682f8f202f9
abinash2055 <abinashnathpandey@gmail.com>
2026-05-17 12:56:54 +0545

    project done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import TaskListPage from './pages/TaskListPage';
--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -3,8 +3,6 @@ import { Link, NavLink, useLocation } from 'react-router-dom';
 import { RouteIndex, RouteTaskList } from '../helper/RouteName';
 
 const Navigation = () => {
-  // const location = useLocation();
-
   const buttonClass =
     'py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100';
 
